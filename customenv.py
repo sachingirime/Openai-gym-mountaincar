ed4198a2f2a32783137eb8f32e0a9c576a319e14
sachin giri <sachingiri@sachins-MacBook-Pro.local>
2022-06-30 09:38:48 +0545

    final chopper rendering

--- customenv.py
+++ customenv.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import PIL.Image as Image
 import gym
 import random
+from IPython import display
 
 from gym import Env, spaces
 import time
@@ -25,6 +26,8 @@ class ChopperScape(Env):
         
         # Define an action space ranging from 0 to 4
         self.action_space = spaces.Discrete(6,)
+
+
                         
         # Create a canvas to render the environment images upon 
         self.canvas = np.ones(self.observation_shape) * 1
@@ -40,6 +43,10 @@ class ChopperScape(Env):
         self.x_min = 0
         self.y_max = int (self.observation_shape[0] * 0.9)
         self.x_max = self.observation_shape[1]
+
+
+
+
 
     def draw_elements_on_canvas(self):
         # Init the canvas 
@@ -97,9 +104,139 @@ class ChopperScape(Env):
         
         elif mode == "rgb_array":
             return self.canvas
+
+
     
     def close(self):
         cv2.destroyAllWindows()
+
+    def get_action_meanings(self):
+        return {0: "Right", 1: "Left", 2: "Down", 3: "Up", 4: "Do Nothing"}
+
+    def has_collided(self, elem1, elem2):
+        x_col = False
+        y_col = False
+
+        elem1_x, elem1_y = elem1.get_position()
+        elem2_x, elem2_y = elem2.get_position()
+
+        if 2 * abs(elem1_x - elem2_x) <= (elem1.icon_w + elem2.icon_w):
+            x_col = True
+
+        if 2 * abs(elem1_y - elem2_y) <= (elem1.icon_h + elem2.icon_h):
+            y_col = True
+
+        if x_col and y_col:
+            return True
+
+        return False
+
+    def step(self, action):
+        # Flag that marks the termination of an episode
+        done = False
+        
+        # Assert that it is a valid action 
+        assert self.action_space.contains(action), "Invalid Action"
+
+        # Decrease the fuel counter 
+        self.fuel_left -= 1 
+        
+        # Reward for executing a step.
+        reward = 1      
+
+        # apply the action to the chopper
+        if action == 0:
+            self.chopper.move(0,5)
+        elif action == 1:
+            self.chopper.move(0,-5)
+        elif action == 2:
+            self.chopper.move(5,0)
+        elif action == 3:
+            self.chopper.move(-5,0)
+        elif action == 4:
+            self.chopper.move(0,0)
+
+        # Spawn a bird at the right edge with prob 0.01
+        if random.random() < 0.01:
+            
+            # Spawn a bird
+            spawned_bird = Bird("bird_{}".format(self.bird_count), self.x_max, self.x_min, self.y_max, self.y_min)
+            self.bird_count += 1
+
+            # Compute the x,y co-ordinates of the position from where the bird has to be spawned
+            # Horizontally, the position is on the right edge and vertically, the height is randomly 
+            # sampled from the set of permissible values
+            bird_x = self.x_max 
+            bird_y = random.randrange(self.y_min, self.y_max)
+            spawned_bird.set_position(self.x_max, bird_y)
+            
+            # Append the spawned bird to the elements currently present in Env. 
+            self.elements.append(spawned_bird)    
+
+        # Spawn a fuel at the bottom edge with prob 0.01
+        if random.random() < 0.01:
+            # Spawn a fuel tank
+            spawned_fuel = Fuel("fuel_{}".format(self.bird_count), self.x_max, self.x_min, self.y_max, self.y_min)
+            self.fuel_count += 1
+            
+            # Compute the x,y co-ordinates of the position from where the fuel tank has to be spawned
+            # Horizontally, the position is randomly chosen from the list of permissible values and 
+            # vertically, the position is on the bottom edge
+            fuel_x = random.randrange(self.x_min, self.x_max)
+            fuel_y = self.y_max
+            spawned_fuel.set_position(fuel_x, fuel_y)
+            
+            # Append the spawned fuel tank to the elemetns currently present in the Env.
+            self.elements.append(spawned_fuel)   
+
+        # For elements in the Ev
+        for elem in self.elements:
+            if isinstance(elem, Bird):
+                # If the bird has reached the left edge, remove it from the Env
+                if elem.get_position()[0] <= self.x_min:
+                    self.elements.remove(elem)
+                else:
+                    # Move the bird left by 5 pts.
+                    elem.move(-5,0)
+                
+                # If the bird has collided.
+                if self.has_collided(self.chopper, elem):
+                    # Conclude the episode and remove the chopper from the Env.
+                    done = True
+                    reward = -10
+                    self.elements.remove(self.chopper)
+
+            if isinstance(elem, Fuel):
+                # If the fuel tank has reached the top, remove it from the Env
+                if elem.get_position()[1] <= self.y_min:
+                    self.elements.remove(elem)
+                else:
+                    # Move the Tank up by 5 pts.
+                    elem.move(0, -5)
+                    
+                # If the fuel tank has collided with the chopper.
+                if self.has_collided(self.chopper, elem):
+                    # Remove the fuel tank from the env.
+                    self.elements.remove(elem)
+                    
+                    # Fill the fuel tank of the chopper to full.
+                    self.fuel_left = self.max_fuel
+        
+        # Increment the episodic return
+        self.ep_return += 1
+
+        # Draw elements on the canvas
+        self.draw_elements_on_canvas()
+
+        # If out of fuel, end the episode.
+        if self.fuel_left == 0:
+            done = True
+
+        return self.canvas, reward, done, []
+
+        
+
+
 
 
 class Point(object):
@@ -137,6 +274,9 @@ class Chopper(Point):
         self.icon_w = 64
         self.icon_h = 64
         self.icon = cv2.resize(self.icon, (self.icon_h, self.icon_w))
+    
+
+        
 
     
 class Bird(Point):
@@ -160,8 +300,25 @@ class Fuel(Point):
 
 
 
+# env = ChopperScape()
+# obs = env.reset()
+# screen = env.render(mode = "rgb_array")
+# plt.imshow(screen)
+# plt.show()
+
 env = ChopperScape()
 obs = env.reset()
-screen = env.render(mode = "rgb_array")
-plt.imshow(screen)
-plt.show()
+
+
+while True:
+    # Take a random action
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    
+    # Render the game
+    env.render()
+    
+    if done == True:
+        break
+
+env.close()
